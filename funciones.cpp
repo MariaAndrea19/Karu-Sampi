f91593b5a6ea5d32227cf5354bc4334fd00fa3f7
MariaAndrea19 <56844965+MariaAndrea19@users.noreply.github.com>
2019-10-21 18:09:16 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -1,1 +1,12 @@
 include "funciones.h"
+
+    
+Enfermedad Gripe(1, sintomas_gripe);
+sintomas_gripe = 
+    
+    
+    
+Enfermedad::Enfermedad(int type_enf, vector<Sintoma> symp_enf){
+    tipo_enf = type_enf;
+    sintomas_enf = symp_enf;    
+}
