bc1613674e8dd3126545f6b1aeebff71fc4ad6de
MariaAndrea19 <56844965+MariaAndrea19@users.noreply.github.com>
2019-10-21 14:31:44 -0500

    Create funciones.h

---
+++ funciones.h
@@ -0,0 +1,37 @@
+#ifndef FUNCIONES_H
+#define FUNCIONES_H
+
+using namespace std;
+
+class Enfermedad{
+  private:
+    
+  public:
+  
+ };
+ 
+ 
+ class Sintoma{
+  private:
+  
+  public:
+  
+};
+
+
+class Persona{
+  private:
+  
+  public:
+  
+};
+
+
+class Red{
+  private:
+  
+  public:
+  
+};
+
+#endif 
