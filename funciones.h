a0ba08a5d989db57de8c07d42d8d88fb341e6da0
MariaAndrea19 <56844965+MariaAndrea19@users.noreply.github.com>
2019-10-21 15:02:52 -0500

    Update funciones.h

--- funciones.h
+++ funciones.h
@@ -1,21 +1,29 @@
 #ifndef FUNCIONES_H
 #define FUNCIONES_H
 
+#include <vector>
 using namespace std;
 
 class Enfermedad{
   private:
-    
+    int tipo_enfermedad;
+    vector<Sintoma> sintomas_enf;
   public:
-  
+    //constructor
+    Enfermedad();
+    //metodos de acceso
+    bool setSintomas_enf();
+    void getSintomas_enf(); 
  };
  
  
  class Sintoma{
   private:
-  
+    int tipo_sintoma;
+    bool estado_sintoma;
   public:
-  
+    bool setEstado_sintoma();
+    void getEstado_sintoma();
 };
 
 
