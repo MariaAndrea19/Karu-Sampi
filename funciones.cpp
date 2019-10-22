dbe315fcaf5aa3ce0bae64dfc0ca4a60ce0b219c
MariaAndrea19 <56844965+MariaAndrea19@users.noreply.github.com>
2019-10-21 20:44:23 -0500

    Update funciones.cpp

--- funciones.cpp
+++ funciones.cpp
@@ -2,8 +2,9 @@ include "funciones.h"
 
     
 Enfermedad Gripe(1, sintomas_gripe);
-sintomas_gripe = 
-    
+vector<Sintoma> sintomas_gripe = {dolor_garganta, dolor_cabeza, agotamiento_extremo, fiebre, fatiga, dolor_muscular, rapidez, tos, congestion};
+vector<Sintomas> sintomas_resfrio = {dolor_garganta, dolor_cabeza, estornudos, estornudos_ratio, congestion, espesor_mucosidad};
+vector<Sintomas> sintomas_alergia = {};
     
     
 Enfermedad::Enfermedad(int type_enf, vector<Sintoma> symp_enf){
