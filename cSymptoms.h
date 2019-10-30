471f6286be48a7de5087f138817cf55ab495c6e6
MariaAndrea19 <56844965+MariaAndrea19@users.noreply.github.com>
2019-10-30 12:11:33 -0500

    Update and rename cSymptoms to cSymptoms.h

---
+++ cSymptoms.h
@@ -0,0 +1,15 @@
+#ifndef KARU_SAMPI_CSYMPTOMS_H
+#define KARU_SAMPI_CSYMPTOMS_H
+
+ class cSymptom{
+    private:
+        bool symptomState;
+    public:
+        symptom(bool symptomState);
+        void setSymptomState();
+        bool getSymptomState();
+};
+
+
+
+#endif
