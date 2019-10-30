554c4079ed0d2921a1dada50bf84c2357da285e0
MariaAndrea19 <56844965+MariaAndrea19@users.noreply.github.com>
2019-10-30 12:19:54 -0500

    Update cDisease.h

--- cDisease.h
+++ cDisease.h
@@ -1,6 +1,9 @@
 #ifndef KARU_SAMPI_CDISEASE_H
 #define KARU_SAMPI_CDISEASE_H
 
+#include "variables.h"
+#include "cSymptoms.h"
+
 class cDisease{
     private:
         vector<cSymptom> diseaseSymptom;
