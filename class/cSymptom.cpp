3c45a4636ad8e8fa7f0a406c11e8f18784d614ee
MariaAndrea19 <56844965+MariaAndrea19@users.noreply.github.com>
2019-11-01 22:41:02 -0500

    Update cSymptom.cpp

--- class/cSymptom.cpp
+++ class/cSymptom.cpp
@@ -8,8 +8,10 @@ cSymptom::cSymptom(text_t n, state_t p, days_t t): name(n), presence(p), time(t)
 void cSymptom::setName(text_t n) {name = n;}
 void cSymptom::setPresence(state_t p) {presence = p;}
 void cSymptom::setTime(days_t t) {time = t;}
+void cSymptom::setIntensity(intensity_t i) {intensity = i}
 
 text_t  cSymptom::getName(){return name;}
 state_t cSymptom::getPresence(){return presence;}
 days_t  cSymptom::getTime(){return time;}
+intensity_t cSymptom::getIntensity(){return intensity;}
 
