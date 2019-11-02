d68f1abd7b01ee4b54c147372dc312e24bc1c0ca
MariaAndrea19 <56844965+MariaAndrea19@users.noreply.github.com>
2019-11-01 23:27:24 -0500

    Create cReport.h

---
+++ class/cReport.h
@@ -0,0 +1,19 @@
+#ifndef KARU-SAMPI-CLASS-CREPORT-H
+#define KARU-SAMPI-CLASS-CREPORT-H
+
+#include "..\types.h"
+#include "cMedicine.h"
+#include "cPatient.h"
+#include "cDisease.h"
+#include "cSpecialist.h"
+
+class cReport{
+    private:
+        
+    public:
+        
+
+
+};
+
+#endif
