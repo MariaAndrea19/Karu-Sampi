854fb5c9ee0df2c409fbd065b36ecaea7abd72ce
Eduardo Castro Quispe <eduardo.castro@utec.edu.pe>
2019-11-01 22:15:01 -0500

    Update

--- class/cSymptom.h
+++ class/cSymptom.h
@@ -6,22 +6,22 @@
 class cSymptom{
         text_t name;
         state_t presence;
-        days_t time; 
         intensity_t intensity;
-        
+        days_t time; 
+                
     public:
         cSymptom();
         cSymptom(text_t n, state_t p, days_t t);
  
         void setName(text_t n);
         void setPresence(state_t p);
-        void setTime(days_t t);
         void setIntensity(intensity_t i);
-
+        void setTime(days_t t);
+       
         text_t getName();
         state_t getPresence();
-        days_t getTime();
         intensity_t getIntensity();
+        days_t getTime();
 };
 
 #endif
