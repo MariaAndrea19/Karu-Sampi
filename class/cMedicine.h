c532e1e963e9f2adab47c2a26bf40cd97ed4f86e
MariaAndrea19 <56844965+MariaAndrea19@users.noreply.github.com>
2019-11-01 23:36:20 -0500

    Update cMedicine.h

--- class/cMedicine.h
+++ class/cMedicine.h
@@ -8,14 +8,18 @@
 class cMedicine{
     private:
         text_t name;
-
+        time_t time;
+        text_t disease;
+    
     public:
         cMedicine();
         ~cMedicine();
 
         void setName(text_t n);
-
+    
         text_t getName();
+        time_t getTime();
+        text_t getDisease();
 
 };
 
