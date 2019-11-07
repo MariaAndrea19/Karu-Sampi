fe51d1f4bd394b7d325a3b61af63b7a7333e2b5d
Eduardo Castro Quispe <eduardo.castro@utec.edu.pe>
2019-11-07 17:37:02 -0500

    Update

--- class/cSpecialist.cpp
+++
@@ -1,9 +0,0 @@
-#include "cSpecialist.h"
-
-cSpecialist::cSpecialist(): name(""), disease(""), intensity(0) {}
-
-void cSpecialist::setName(text_t n) {name = n;}
-
-text_t cSpecialist::getName(){return name;}
-text_t cSpecialist::getDisease(){return disease;}
-intensity_t cSpecialist::getIntensity(){return intensity;}
