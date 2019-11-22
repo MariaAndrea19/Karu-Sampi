0f30f9df9507feb181ead2106d852706c89ab2a5
Liilo <55290847+Liilo@users.noreply.github.com>
2019-11-22 12:20:57 -0500

    Update cMedicine.cpp

--- class/cMedicine.cpp
+++ class/cMedicine.cpp
@@ -15,4 +15,12 @@ text_t cMedicine::getDisease(){return disease;}
 //Niño(6-14 años, con índice de masa corporal de 18.00-24.99): antihistamínico: leve: cetirizina
 //(de 10 mg, tableta 10 comprimidos.), intenso: hisaler (laboratorio: bago, es más costoso S/1.26 solo un comprimido) 
 //Adulto (15- adelante): dependiendo de la intensidad: leve: cetirizina, loratadina. intenso: hisaler, levocetirizina.
-
+//Resfriado: no se tiene tratamiento directo ya que es virico. 
+//niños mayores de 2 años: unguento mentolado. en caso de fiebre: ibuprofeno, acetaminofen, gotas nasales para obstruccion nasal, más de seis años, antihistaminico.
+//niños menores de 2 años pueden tomar ibuprofeno en caso de fiebre. en caso de infeccion, paracetamol 
+//aultos(15-adelante): unguento mentolado, ibuprofeno para aliviar dolor, amoxicilina con acido clavulanico(INFECCION), cetirizina/levo congestion nasal.
+//Gripe:
+//niños menores de dos años: tamiflu + tylenol (fiebre)
+//niños(2-14): ibuprofeno/paracetamol (dolor, fiebre)
+//dultos:  antihisstaminicos (CETI, LEVO), ibuprofeno, naproxeno, paracetamol (dolor, fiebre), en caso de proceso bacteriano, antibiotico.
+//en caso de tos seca, jarabe (abrilar, abriflu)
