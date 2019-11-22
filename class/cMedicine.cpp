27c1ef105ca03fafb746d8512c2fc29b132f113e
Liilo <55290847+Liilo@users.noreply.github.com>
2019-11-22 11:53:17 -0500

    Update cMedicine.cpp

--- class/cMedicine.cpp
+++ class/cMedicine.cpp
@@ -8,3 +8,11 @@ text_t cMedicine::getName(){return name;}
 time_t cMedicine::getTime(){return time;}
 text_t cMedicine::getDisease(){return disease;}
 
+
+//Medicinas a emplear:
+//Alergia
+//Infante 0-5 años = cetirizina 5 mg jarabe. S/3.20, una cucharadita.
+//Niño(6-14 años, con índice de masa corporal de 18.00-24.99): antihistamínico: leve: cetirizina
+//(de 10 mg, tableta 10 comprimidos.), intenso: hisaler (laboratorio: bago, es más costoso S/1.26 solo un comprimido) 
+//Adulto (15- adelante): dependiendo de la intensidad: leve: cetirizina, loratadina. intenso: hisaler, levocetirizina.
+
