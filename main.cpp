4891538749273b3f085cd8b7c704ac457b981d6a
MariaAndrea19 <56844965+MariaAndrea19@users.noreply.github.com>
2019-11-22 12:16:58 -0500

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -1,15 +1,39 @@
 #include <iostream>
-#include "funciones.h"
+#include "CSymptom.h"
+#include "CDisease.h"
+#include "CPatient.h"
+#include "types.h"
+#include "cMedicine.h"
+#include "cReport.h"
 
 
 int main(){ 
-    inicializar_sintomas();
+    //Initializing objects
+    CSymptom.initializeSymptoms();
+    CMedicine.initializeMedicines();    
+    CDisease.initializeDisease();
     
-    //RED NEURONAL
-    
-    inicializar_enfermedades();
-
-    //CONTEO DE PROBABILIDADES +
+    //Patient data
+    CPatient Patient;
+    cout<<"¿Cómo se llama?"<<endl;
+    cin>>name;
+    cout<<"¿Cuántos años tiene?"<<endl;
+    cin>>age;
+    cout<<"¿Cuál es su género? (escriba M/F)"<<endl;
+    cin>>gender;
+    cout<<"¿Cuánto pesa?"<<endl;
+    cin>>mass;
+    cout<<"¿Cuánto mide?"<<endl;
+    cin>>height;
+    cout<<"Ingrese su presión"<<endl;
+    cin>>pressure;
+    cout<<"Ingrese su ritmo cardiaco"<<endl;
+    cin>>heartRate;
+  
+    Patient(n,a,g,m,h,p,hR);
  
+    
+    
+    
   return 0;
 }
