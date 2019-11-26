e601407c66a672f2202293a6e1c819ce361470ad
Eduardo Castro Quispe <eduardo.castro@utec.edu.pe>
2019-11-26 10:33:21 -0500

    Update

--- class/cPatient.cpp
+++ class/cPatient.cpp
@@ -1,21 +1,2 @@
 #include "cPatient.h"
 
-cPatient::cPatient(): name(""), gender(true), mass(0), height(0), pressure(0), heartRate(0) {}
-
-cPatient::cPatient(text_t n, gender_t g, kilogram_t m, meter_t h, mmhg_t p, bpm_t hR): name(n), gender(g), mass(m), height(h), pressure(p), heartRate(hR) {}
-
-
-void cPatient::setName(text_t n)      {name = n      ;}
-void cPatient::setGender(gender_t g)  {gender = g    ;}
-void cPatient::setMass(kilogram_t m)  {mass = m      ;}
-void cPatient::setHeight(meter_t h)   {height = h    ;}
-void cPatient::setPressure(mmhg_t p)  {pressure = p  ;}
-void cPatient::setHeartRate(bpm_t hR) {heartRate = hR;}
-
-text_t     cPatient::getName() {return name;}
-gender_t   cPatient::getGender() {return gender;}
-kilogram_t cPatient::getMass() {return mass;}
-meter_t    cPatient::getHeight() {return height;}
-mmhg_t     cPatient::getPressure() {return pressure;}
-bpm_t      cPatient::getHeartRate() {return heartRate;}
-
--- class/cPatient.h
+++ class/cPatient.h
@@ -2,6 +2,9 @@
 #define KARU_SAMPI_CLASS_CPATIENT_H
 
 #include "..\types.h"
+#include "cDisease.h"
+
+using diseases_t = vector<cDisease>;
 
 class cPatient {
     private:
@@ -12,25 +15,28 @@ class cPatient {
         meter_t height;
         mmhg_t pressure;
         bpm_t heartRate;
+        diseases_t diseases;
     public:
-        cPatient();
-        cPatient(text_t n, age_t a,gender_t g, kilogram_t m, meter_t h, mmhg_t p, bpm_t hR);
+        cPatient(): name(""), age(0), gender(true), mass(0), height(0), pressure(0), heartRate(0) {}
+        cPatient(text_t n, age_t a, gender_t g, kilogram_t m, meter_t h, mmhg_t p, bpm_t hR): name(n), age(a), gender(g), mass(m), height(h), pressure(p), heartRate(hR) {}
 
-        void setName(text_t n);
-        void setAge(age_t a);
-        void setGender(gender_t g);
-        void setMass(kilogram_t m);
-        void setHeight(meter_t h);
-        void setPressure(mmhg_t p);
-        void setHeartRate(bpm_t hR);
+        void setName(text_t n){name = n;} 
+        void setAge(age_t a){age = a;}
+        void setGender(gender_t g){gender = g;}
+        void setMass(kilogram_t m){mass = m;}
+        void setHeight(meter_t h){height = h;}
+        void setPressure(mmhg_t p){pressure = p;}
+        void setHeartRate(bpm_t hR){heartRate = hR;}
+        void addDisease(cDisease d){diseases.emplace_back(d);}
 
-        text_t getName();
-        age_t getAge();
-        gender_t getGender();
-        kilogram_t getMass();
-        meter_t getHeight();
-        mmhg_t getPressure();
-        bpm_t getHeartRate();
+        text_t getName(){return name;}
+        age_t getAge(){return age;}
+        gender_t getGender(){return gender;}
+        kilogram_t getMass(){return mass;}
+        meter_t getHeight(){return height;}
+        mmhg_t getPressure(){return pressure;}
+        bpm_t getHeartRate(){return heartRate;}
+        diseases_t getDisease(){return diseases;}
 
 };
 
