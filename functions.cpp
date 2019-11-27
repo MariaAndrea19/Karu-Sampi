f69d0029f9523cd0995489401dd8b95695ebbf66
Liilo <55290847+Liilo@users.noreply.github.com>
2019-11-27 13:05:29 -0500

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -3,7 +3,7 @@ double IMC;
 
 void introduction(text_t& name, age_t& age, gender_t& gender, kilogram_t& weight, meter_t& height, mmhg_t& pressure, bpm_t& rate){
     cout<< "¡Hola! Mi nombre es Karu, tu asistente médico. ¿Podrías decirme tu nombre?";
-    cin>> nombre;
+    cin>> name;
     cout<< "Muy bonito nombre." << "¿Cuántos años tienes?";
     cin>> age;
     cout<< "¿Cuánto pesas y cuánto mides? ¡Sin mentir!";
@@ -15,19 +15,37 @@ void introduction(text_t& name, age_t& age, gender_t& gender, kilogram_t& weight
     cout << "¡Increíble! Muchas gracias por responder. Voy a procesar alguno de tus datos.";
 }
 
-/*   IMC = weight/pow(height, 2);
-    
-    if(IMC>24.99)
-        cout << "¡Uy! Antes de empezar, ¡ten cuidado con tu alimentación! Al parecer tienes más peso de lo que deberías. ¡Cuida tus comidas!";
-    else if(IMC<18) cout<<"¡Uy! Antes de empezar, ¡ten cuidado con tu alimentación! Al parecer tienes menos peso de lo que deberías. ¡Cuida tus comidas!";
-    
-    else cout<<"¡Te encuentras en tu peso! Felicitaciones, "<< nombre <<".";
-   
 
-    //CONTEO DE PROBABILIDADES +
+void abnormal (kilogram_t weight, meter_t height, mmgh_t pressure, bpm_t rate){ 
+    imc_t imc;
+    imc = weight/pow(height, 2);
+    
+    if(imc>24.99)
+        cout << "¡Uy! Antes de empezar, ¡ten cuidado con tu alimentación! Al parecer tienes más peso de lo que deberías. ¡Cuida tus comidas!";
+    else if(imc<18) cout<<"¡Uy! Antes de empezar, ¡ten cuidado con tu alimentación! Al parecer tienes menos peso de lo que deberías. ¡Cuida tus comidas!";
+    
+    else cout<<"¡Te encuentras en tu peso! Felicitaciones, "<< name <<".";
+   
+    if(pressure_s>120 || pressure_d>80) cout<< "Oye, ¡ten mucho cuidado! Al parecer tu presión arterial supera los valores normales. Sería peligroso que desarrolles hipertensión. ¡Cuida tu dieta!";
+    else cout<<"¡Perfecta presión! Te encuentras en tus valores normales, ¡eres genial!";
+    
+    if(age<5){
+        if(rate>75 && rate<115){ cout<< "El pequeño tiene un corazón sano. ¡Muy bien!";
+    }else{ cout<< "Necesita dar un chequeo a esos latidos, ¡mantenga sano su corazón!";
+    }else if(age>4 || age<10){
+            if(rate>70 || rate<110){cout<<"El pequeño tiene un corazón sano. ¡Muy bien!";
+                                   }else{cout<<"Necesita dar un chequeo a esos latidos, ¡mantenga sano su corazón!";}
+            else{if(rate>60 || rate<100) cout<<"Tiene un corazón muy sano, ¡increíble!"; else cout<< "Necesita dar un chequeo a esos latidos, ¡mantenga sano su corazón!";
+                }
     cout<< "¡Gracias por ser sincero!";
+        }
+        
+        
+void disease(){
+    
+    //CONTEO DE PROBABILIDADES +
     cout<<"Ahora, ¿puedes contarme sobre tus síntomas," <<nombre<<"?";
-        inicializar_sintomas();
+    inicializar_sintomas();
     
     //RED NEURONAL
     
@@ -38,5 +56,5 @@ void introduction(text_t& name, age_t& age, gender_t& gender, kilogram_t& weight
     cout<< "Si con esto no presentas mejora, ¡no te asustes!, solo dale una visita a este especialista"<< (doctor) <<endl<<
         "Ellos sabrán cómo ayudarte, ¡Tú tranquilo!";
     cout<< "Eso sería todo, " << nombre <<". Muchas gracias por confiar en mí, estoy preparado para ayudarte. Muchísima suerte y que te mejores muy pronto. ¡Adiós!";
-} */
+} 
 
