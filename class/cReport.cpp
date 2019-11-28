fe30e15ea4d634518f045da1d29b5fd23bfd2785
Liilo <55290847+Liilo@users.noreply.github.com>
2019-11-28 15:09:17 -0500

    Update cReport.cpp

--- class/cReport.cpp
+++ class/cReport.cpp
@@ -1,1 +1,24 @@
 #include "cReport.h"
+
+cReport::cReport();
+cReport::~cReport();
+
+void cReport:: getPatient(cPatient* pat){
+  //name->getName;
+  //age->getAge;
+  //gender->getGender;
+  //mass->getMass;
+  //height->getHeight;
+  //pressure->getPressure;
+  //rate->getHeartRate;
+  
+}
+void cReport:: setMedicine (cMedicine* med){
+medicine=med;
+}
+void cReport:: setDoctor(cDoctor* doc){
+doctor = doc;
+}
+void cReport:: showReport(cPatient* pat, cMedicine* med, cDoctor* doc){
+
+}
